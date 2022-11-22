8e38887f8c36720d6f32c30829b860596fa99d73
Om Gadewar <om.gadewar@Yash.com>
2022-11-22 17:46:29 +0530

    first commit this repo

---
+++ src/com/equals/main.java
@@ -0,0 +1,25 @@
+package com.equals;
+
+public class main {
+	
+	
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setId(10);
+		emp.setName("om");
+		
+		Employee emp1 = new Employee();
+		emp1.setId(10);
+		emp1.setName("om");
+		
+		System.out.println(emp.hashCode());
+		
+		System.out.println(emp1.hashCode());
+		
+		System.out.println("Shallow compare : "+(emp==emp));
+		
+		System.out.println("Deep compare :"+ (emp.equals(emp1)));
+	}
+
+}
